77e2c64bcd415f221872fa380f86fa067804f443
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-05 02:46:18 +0900

    different ways to found out palindrome

--- PalindromeCheck/main.cpp
+++ PalindromeCheck/main.cpp
@@ -5,11 +5,58 @@
 //  Created by adeeb mohammed on 05/01/20.
 //  Copyright © 2020 adeeb mohammed. All rights reserved.
 //
-
+//  program to check whether a string is palindrome or not
+// aha is a palindrome
+// alaa is not a palindrome
 #include <iostream>
+#include <string>
 
+
+// solution using recursion
+// o(n) time o(n) space
+// tail recursaion is used so each recursive function is replaced by the last call o(1)
+bool palHelper(std::string str, int i){
+    int j = (int)str.length() - 1 - i;
+    return (i >= j) ? true: (str[i] == str[j]) && palHelper(str,i+1);
+}
+bool isPalindromeRecursion(std::string str){
+    return palHelper(str,0);
+}
+
+// more cleaner code using
+bool isPalindrome(std::string str){
+    int leftIndex = 0;
+    int rightIndex = (int)str.size()-1;
+    while(leftIndex < rightIndex){
+        if(str.at(leftIndex) != str.at(rightIndex)){
+            return false;
+        }
+        leftIndex++;
+        rightIndex--;
+    };
+    return true;
+}
+
+// solution using for loop
+bool isPalindromeOrg(std::string str) {
+    int szStr = (int)str.size();
+    if(str.empty()){
+        return false;
+    }else if(szStr == 1){
+        return true;
+    }else {
+        for(int i = 0, j = (szStr -1) ; i <= (szStr/2) , j >=(szStr/2) ;j--, i++){
+            if(str[i] != str[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+  return false;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Program to check palindrome "<< std::endl;
+    std::string str = "aha";
+    std::cout << "palindrome :" << isPalindromeRecursion(str) << std::endl;
     return 0;
 }
